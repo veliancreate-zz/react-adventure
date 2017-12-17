5dbe1e7e59e7dd6d0657e321c5b22c5118bb8004
Julian Veling <julian_veling@hotmail.com>
2017-12-17 14:47:37 +0000

    init

---
+++ src/client/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Heros</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
